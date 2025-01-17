07d8fddfd13634c5b6a969489d40b686f6cf8c09
shamy <shamyrquishpet@gmail.com>
2025-01-16 19:28:32 -0500

    Estructura base del proyecto

---
+++ app/src/main/java/com/grupo3/gravimetro/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.grupo3.gravimetro
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
